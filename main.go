91204ea32fd057ffa6b427c8236099c34c052409
wignn <tigfiragnafatur1933@gmail.com>
2025-11-21 09:28:27 +0700

    Refactor Makefile and Docker Compose; update dependencies and add CORS middleware

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"time"
 	"vasvault/internal/repositories"
 	"vasvault/internal/routes"
-	"fmt"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-   		fmt.Println("No .env file found, using system environment")
+		fmt.Println("No .env file found, using system environment")
 	}
-
 	db, err := repositories.Connect()
 	if err != nil {
 		panic("Failed to connect to database")
 	}
 
 	r := gin.Default()
+	
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	routes.InitRoutes(r, db.DB)
 
 	if err := r.Run(); err != nil {
